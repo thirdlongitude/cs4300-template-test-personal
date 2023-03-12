e711511c85f1f6b15fd6ae50c0aed2e2d8408d71
Mayank Rao <ms3293@cornell.edu>
2023-03-12 19:57:21 -0400

    SQL engine

--- backend/helpers/MySQLDatabaseHandler.py
+++ backend/helpers/MySQLDatabaseHandler.py
@@ -2,9 +2,7 @@ import os
 import sqlalchemy as db
 from dotenv import load_dotenv
 from pathlib import Path
-
-dotenv_path = Path(os.environ['ROOT_PATH'])
-load_dotenv(dotenv_path=dotenv_path)
+load_dotenv(dotenv_path=os.path.join("..",".."))
 
 class MySQLDatabaseHandler(object):
     
