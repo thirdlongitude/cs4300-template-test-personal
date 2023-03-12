4a241c0a69eac2779b0ea88aa960b6340a595f78
Mayank Rao <ms3293@cornell.edu>
2023-03-12 18:23:33 -0400

    SQL engine

--- backend/helpers/MySQLDatabaseHandler.py
+++ backend/helpers/MySQLDatabaseHandler.py
@@ -11,31 +11,29 @@ class MySQLDatabaseHandler(object):
         self.MYSQL_PORT = MYSQL_PORT
         self.MYSQL_DATABASE = MYSQL_DATABASE
         self.engine = self.validate_connection()
+        self.conn = None
 
     def validate_connection(self):
 
         engine = db.create_engine(f"mysql+pymysql://{self.MYSQL_USER}:{self.MYSQL_USER_PASSWORD}@{self.MYSQL_HOST}:{self.MYSQL_PORT}")
-        conn = engine.connect()
-        conn.execute(f"CREATE DATABASE IF NOT EXISTS {self.MYSQL_DATABASE}")
-        conn.execute(f"USE {self.MYSQL_DATABASE}")
+        self.conn = engine.connect()
+        self.conn.execute(f"CREATE DATABASE IF NOT EXISTS {self.MYSQL_DATABASE}")
+        self.conn.execute(f"USE {self.MYSQL_DATABASE}")
         return engine
 
-    def lease_connection(self):
-        return self.engine.connect()
+
     
     def query_executor(self,query):
-        conn = self.lease_connection()
         if type(query) == list:
             for i in query:
-                conn.execute(i)
+                self.conn.execute(i)
         else:
-            conn.execute(query)
+            self.conn.execute(query)
         
 
     def query_selector(self,query):
-        conn = self.lease_connection()
         print("QUERY FOR SQL",type(query))
-        data = conn.execute(query)
+        data = self.conn.execute(query)
         return data
 
     def load_file_into_db(self,file_name = os.path.join("..","init.sql")):
